0c9c763af567be44d8a8884ff977ab81f0416920
Dannong Wang <wangd12@rpi.edu>
2024-10-18 16:17:46 -0400

    update train-lora code

--- train_lora.py
+++ train_lora.py
@@ -1,159 +1,130 @@
 import os
-# os.environ['CUDA_VISIBLE_DEVICES'] = '0,1,2,3'
-from transformers.integrations import TensorBoardCallback
-from transformers import AutoTokenizer, AutoModel  # Model,Tokenizer
-from transformers import DataCollatorForLanguageModeling  # Datacollator
-from transformers import TrainingArguments, Trainer
-from transformers import AutoModel, AutoTokenizer, LlamaForCausalLM, LlamaTokenizerFast
-from transformers import BitsAndBytesConfig
-from deepspeed.pipe import PipelineModule
-from torch.utils.tensorboard import SummaryWriter
+import sys
+import argparse
+from datetime import datetime
+from functools import partial
 import datasets
 import torch
-import pdb
-
-local_rank = int(os.environ["LOCAL_RANK"])
-# LoRA
+from torch.utils.tensorboard import SummaryWriter
+import wandb
+from transformers import (
+    AutoTokenizer,
+    AutoModel,
+    AutoModelForCausalLM,
+    TrainingArguments,
+    Trainer,
+    DataCollatorForSeq2Seq
+)
+from transformers.trainer import TRAINING_ARGS_NAME
+from transformers.integrations import TensorBoardCallback
+# Importing LoRA specific modules
 from peft import (
     TaskType,
     LoraConfig,
     get_peft_model,
     get_peft_model_state_dict,
-    prepare_model_for_kbit_training,
-    set_peft_model_state_dict,
+    set_peft_model_state_dict
 )
-
-# Model,Tokenizer, Datacollator
-model_name = "meta-llama/Llama-3.1-8B-Instruct"
-# model_name = '/colab_space/yanglet/models--daryl149--Llama-2-7b-chat-hf/snapshots/bbc9b373dacff93e600e4426f2b3d3dd264e90ed'
-tokenizer = LlamaTokenizerFast.from_pretrained(model_name, trust_remote_code=True)
-tokenizer.pad_token = tokenizer.eos_token
-
-# Trainer
-class ModifiedTrainer(Trainer):
-    def compute_loss(self, model, inputs, return_outputs=False):
-        return model(
-            input_ids=inputs["input_ids"],
-            labels=inputs["labels"],
-        ).loss
-
-    def prediction_step(self, model: torch.nn.Module, inputs, prediction_loss_only: bool, ignore_keys=None):
-        with torch.no_grad():
-            res = model(
-                input_ids=inputs["input_ids"].to(model.device),
-                labels=inputs["labels"].to(model.device),
-            ).loss
-        return (res, None, None)
-
-    def save_model(self, output_dir=None, _internal_call=False):
-        from transformers.trainer import TRAINING_ARGS_NAME
-
-        os.makedirs(output_dir, exist_ok=True)
-        torch.save(self.args, os.path.join(output_dir, TRAINING_ARGS_NAME))
-        saved_params = {
-            k: v.to("cpu") for k, v in self.model.named_parameters() if v.requires_grad
-        }
-        torch.save(saved_params, os.path.join(output_dir, "adapter_model.bin"))
+from utils import *
 
 
-def data_collator(features: list) -> dict:
-    len_ids = [len(feature["input_ids"]) for feature in features]
-    longest = max(len_ids)
-    input_ids = []
-    labels_list = []
-    for ids_l, feature in sorted(zip(len_ids, features), key=lambda x: -x[0]):
-        ids = feature["input_ids"]
-        seq_len = feature["seq_len"]
-        labels = (
-                [tokenizer.pad_token_id] * (seq_len - 1) + ids[(seq_len - 1):] + [tokenizer.pad_token_id] * (
-                    longest - ids_l)
-        )
-        ids = ids + [tokenizer.pad_token_id] * (longest - ids_l)
-        _ids = torch.LongTensor(ids)
-        labels_list.append(torch.LongTensor(labels))
-        input_ids.append(_ids)
-    input_ids = torch.stack(input_ids)
-    labels = torch.stack(labels_list)
-    return {
-        "input_ids": input_ids,
-        "labels": labels,
-    }
+# Replace with your own api_key and project name
+os.environ['WANDB_PROJECT'] = 'fin-lora'
 
 
-class CastOutputToFloat(torch.nn.Sequential):
-    def forward(self, x):
-        return super().forward(x).to(torch.float32)
+def main(args):
+    """
+    Main function to execute the training script.
 
+    :param args: Command line arguments
+    """
 
-def main():
-    # load data
-    # dataset = datasets.load_from_disk("./data/dataset_new")
-    dataset = datasets.load_from_disk("./data/fingpt_data_train_token")
-    dataset = dataset.train_test_split(0.02, shuffle=True, seed=42)
-
-    device_map = "auto"
-    world_size = int(os.environ.get("WORLD_SIZE", 1))
-    # world_size = 1
-    ddp = world_size != 1
-    if ddp:
-        device_map = {"": int(os.environ.get("LOCAL_RANK") or 0)}
-        # gradient_accumulation_steps = gradient_accumulation_steps // world_size
-
-    # config
-    deepspeed_config = "./config_.json"
-    # import deepspeed
-    # deepspeed.init_distributed(dist_backend = "gloo")
-    training_args = TrainingArguments(
-        output_dir='./l-8b-r8-out',
-        # output_dir='./test',
-        logging_steps=200,
-        # max_steps=800,
-        num_train_epochs=2 * 4,
-        per_device_train_batch_size=10,
-        gradient_accumulation_steps=8,
-        learning_rate=1e-4,
-        weight_decay=0.01,
-        warmup_steps=200,
-        save_steps=400,
-        # ddp_backend = 'gloo',
-        fp16=True,
-        # bf16=True,
-        deepspeed=deepspeed_config,
-        torch_compile=False,
-        load_best_model_at_end=True,
-        evaluation_strategy="steps",
-        remove_unused_columns=False,
-        ddp_find_unused_parameters=False if ddp else None,
-        # testing only, comment otherwise
-        dataloader_num_workers=64,
-        dataloader_pin_memory=True
-    )
-
-    # quantization_config = BitsAndBytesConfig(
-    #     load_in_4bit=True,
-    #     bnb_4bit_compute_dtype=torch.bfloat16,
-    #     bnb_4bit_use_double_quant=True,
-    #     bnb_4bit_quant_type='nf4'
-    # )
-
-    quantization_config = BitsAndBytesConfig(load_in_8bit=True)
-
-
-    # load model
-    model = LlamaForCausalLM.from_pretrained(
+    # Parse the model name and determine if it should be fetched from a remote source
+    # model_name = parse_model_name(args.base_model, args.from_remote)
+    model_name = args.base_model
+    # Load the pre-trained causal language model
+    model = AutoModelForCausalLM.from_pretrained(
         model_name,
         # load_in_8bit=True,
-        # load_in_4bit=True,
-        trust_remote_code=True,
-        device_map=device_map,
-        torch_dtype=torch.float16,
-        # quantization_config = quantization_config
-        # cache_dir='/colab_space/yanglet/model_weight/LLM'
+        # device_map="auto",
+        trust_remote_code=True
     )
+    # Print model architecture for the first process in distributed training
+    if args.local_rank == 0:
+        print(model)
 
-    model = prepare_model_for_kbit_training(model)
+    # Load tokenizer associated with the pre-trained model
+    tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
 
-    # setup peft
+    # Apply model specific tokenization settings
+    if args.base_model != 'mpt':
+        tokenizer.padding_side = "left"
+    if args.base_model == 'qwen':
+        tokenizer.eos_token_id = tokenizer.convert_tokens_to_ids('<|endoftext|>')
+        tokenizer.pad_token_id = tokenizer.convert_tokens_to_ids('<|extra_0|>')
+    # Ensure padding token is set correctly
+    if not tokenizer.pad_token or tokenizer.pad_token_id == tokenizer.eos_token_id:
+        tokenizer.add_special_tokens({'pad_token': '[PAD]'})
+        model.resize_token_embeddings(len(tokenizer))
+    
+    # Load training and testing datasets
+    dataset_list = load_dataset(args.dataset, args.from_remote)
+    dataset_train = datasets.concatenate_datasets([d['train'] for d in dataset_list]).shuffle(seed=42)
+    
+    if args.test_dataset:
+        dataset_list = load_dataset(args.test_dataset, args.from_remote)
+    dataset_test = datasets.concatenate_datasets([d['test'] for d in dataset_list])
+    
+    dataset = datasets.DatasetDict({'train': dataset_train, 'test': dataset_test})
+    # Display first sample from the training dataset
+    print(dataset['train'][0])
+    # Filter out samples that exceed the maximum token length and remove unused columns
+    dataset = dataset.map(partial(tokenize, args, tokenizer))
+    print('original dataset length: ', len(dataset['train']))
+    dataset = dataset.filter(lambda x: not x['exceed_max_length'])
+    print('filtered dataset length: ', len(dataset['train']))
+    dataset = dataset.remove_columns(['instruction', 'input', 'output', 'exceed_max_length'])
+    
+    print(dataset['train'][0])
+
+    # Create a timestamp for model saving
+    current_time = datetime.now()
+    formatted_time = current_time.strftime('%Y%m%d%H%M')
+
+    # Set up training arguments
+    training_args = TrainingArguments(
+        output_dir=f'finetuned_models/{args.run_name}_{formatted_time}', 
+        logging_steps=args.log_interval,
+        num_train_epochs=args.num_epochs,
+        per_device_train_batch_size=args.batch_size,
+        per_device_eval_batch_size=args.batch_size,
+        gradient_accumulation_steps=args.gradient_steps,
+        dataloader_num_workers=args.num_workers,
+        learning_rate=args.learning_rate,
+        warmup_ratio=args.warmup_ratio,
+        lr_scheduler_type=args.scheduler,
+        save_steps=args.eval_steps,
+        eval_steps=args.eval_steps,
+        fp16=True,
+        # fp16_full_eval=True,
+        deepspeed=args.ds_config,
+        evaluation_strategy=args.evaluation_strategy,
+        load_best_model_at_end=args.load_best_model,
+        remove_unused_columns=False,
+        report_to='wandb',
+        run_name=args.run_name
+    )
+    if not args.base_model == 'mpt':
+        model.gradient_checkpointing_enable()
+    model.enable_input_require_grads()
+    model.is_parallelizable = True
+    model.model_parallel = True
+    model.config.use_cache = (
+        False
+    )
+    # model = prepare_model_for_int8_training(model
+
+    # setup peft for lora
     peft_config = LoraConfig(
         task_type=TaskType.CAUSAL_LM,
         inference_mode=False,
@@ -163,45 +134,63 @@ def main():
         target_modules=['q_proj', "k_proj", 'v_proj'],
         bias='none',
     )
-
     model = get_peft_model(model, peft_config)
-
-    model.print_trainable_parameters()
-
-    model.gradient_checkpointing_enable()
-    model.enable_input_require_grads()
-    model.is_parallelizable = True
-    model.model_parallel = True
-
-    # KVcache inference
-    model.config.use_cache = (
-        False  # silence the warnings. Please re-enable for inference!
-    )
-
-    # model.config.use_cache = False
-    # old_state_dict = model.state_dict
-    # model.state_dict = (
-    #     lambda self, *_, **__: get_peft_model_state_dict(
-    #         self, old_state_dict()
-    #     )
-    # ).__get__(model, type(model))
-
-    # Train
+    
+    # Initialize TensorBoard for logging
     writer = SummaryWriter()
-    trainer = ModifiedTrainer(
-        model=model,
-        args=training_args,
+
+    # Initialize the trainer
+    trainer = Trainer(
+        model=model, 
+        args=training_args, 
         train_dataset=dataset["train"],
-        eval_dataset=dataset["test"],
-        data_collator=data_collator,
+        eval_dataset=dataset["test"], 
+        data_collator=DataCollatorForSeq2Seq(
+            tokenizer, padding=True,
+            return_tensors="pt"
+        ),
         callbacks=[TensorBoardCallback(writer)],
     )
+    
+    # if torch.__version__ >= "2" and sys.platform != "win32":
+    #     model = torch.compile(model)
 
+    # Clear CUDA cache and start training
+    torch.cuda.empty_cache()
     trainer.train()
     writer.close()
-    # save model
+
+    # Save the fine-tuned model
     model.save_pretrained(training_args.output_dir)
 
 
 if __name__ == "__main__":
-    main()
+    # Argument parser for command line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--local_rank", default=0, type=int)
+    parser.add_argument("--run_name", default='local-test', type=str)
+    parser.add_argument("--dataset", required=True, type=str)
+    parser.add_argument("--test_dataset", type=str)
+    parser.add_argument("--base_model", required=True, type=str)
+    parser.add_argument("--max_length", default=512, type=int)
+    parser.add_argument("--batch_size", default=4, type=int, help="The train batch size per device")
+    parser.add_argument("--learning_rate", default=1e-4, type=float, help="The learning rate")
+    parser.add_argument("--num_epochs", default=8, type=float, help="The training epochs")
+    parser.add_argument("--gradient_steps", default=8, type=float, help="The gradient accumulation steps")
+    parser.add_argument("--num_workers", default=8, type=int, help="dataloader workers")
+    parser.add_argument("--log_interval", default=20, type=int)
+    parser.add_argument("--warmup_ratio", default=0.05, type=float)
+    parser.add_argument("--ds_config", default='./config_new.json', type=str)
+    parser.add_argument("--scheduler", default='linear', type=str)
+    parser.add_argument("--instruct_template", default='default')
+    parser.add_argument("--evaluation_strategy", default='steps', type=str)
+    parser.add_argument("--load_best_model", default='False', type=bool)
+    parser.add_argument("--eval_steps", default=0.1, type=float)    
+    parser.add_argument("--from_remote", default=True, type=bool)    
+    args = parser.parse_args()
+
+    # Login to Weights and Biases
+    wandb.login()
+
+    # Run the main function
+    main(args)
